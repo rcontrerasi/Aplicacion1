caaafa453d688b7a3149d881b67fe34673f5a416
Rodrigo Contreras <rodrigoh.contrerasi@gmail.com>
2020-03-30 02:39:49 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/CursoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicacion1.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string mensaje()
+        {
+            return "Bienvenido a Curso";
+        }
+    }
+}
---
+++ Controllers/SeccionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicacion1.Controllers
+{
+    public class SeccionController : Controller
+    {
+        // GET: Seccion
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
